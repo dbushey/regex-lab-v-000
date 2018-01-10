f0c40739e8c02ee795e9ad71f45be3623547519c
Daiane Bushey <daianebushey@gmail.com>
2018-01-10 04:26:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,6 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  #(?=.*^[A-Z])(?=.*[[:punct:]]$).*
   if text.match(/^[A-Z]/) && text.match(/[[:punct:]]$/)
     return true
   else
