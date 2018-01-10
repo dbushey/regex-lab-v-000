2808bb319fa5c6e9f4eefa00acef179235bd8b66
Daiane Bushey <daianebushey@gmail.com>
2018-01-10 04:23:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,8 +9,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  \ing$
-  text.scan(/\b\^un\b/)
+  #\ing$
+  #text.scan(/\b\^un\b/)
 end
 
 def words_five_letters_long(text)
